6c5bc90f18272d6977a85d3678c09102a4e72df5
stridrollins <stridrollins@gmail.com>
2026-08-17 09:41:30 -1000

    New skills

--- skillfactory.py
+++ skillfactory.py
@@ -10,8 +10,8 @@ LAST_SPURT = 5/6
 
 
 
-
-### VELOCITY ##########################################################
+### VELOCITY ###############################################################################################################################
+############################################################################################################################################
 def corner_adept():
     return Skill(
         "Corner Adept",
@@ -164,7 +164,18 @@ def masterful_gambit():
     return Skill(
         "Masterful Gambit",
         "Standard",
-        EndCloserTrigger() & AfterDistanceTrigger(LAST_SPURT)
+        EndCloserTrigger() & AfterDistanceTrigger(LAST_SPURT),
+        [Velocity(0.5)],
+        duration=3.0
+    )
+
+def sturm_und_drang():
+    return Skill(
+        "Sturm Und Drang",
+        "Rare",
+        EndCloserTrigger() & AfterDistanceTrigger(LAST_SPURT),
+        [Velocity(1)],
+        duration=3.0
     )
 
 def steadfast():
@@ -186,7 +197,189 @@ def unyielding():
     )
 
 
-### ACCEL #############################################################
+
+def up_tempo():
+    return Skill(
+        "Up-Tempo",
+        "Standard",
+        BetweenDistanceTrigger(MID_RACE,LATE_RACE),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+def killer_tunes():
+    return Skill(
+        "Killer Tunes",
+        "Rare",
+        BetweenDistanceTrigger(MID_RACE,LATE_RACE),
+        [Velocity(1)],
+        duration = 3.0
+    )
+
+
+
+
+### STYLE VELOCITY ###############################################################################################################################
+############################################################################################################################################
+
+
+
+
+
+def front_corners():
+    return Skill(
+        "Front Runner Corners",
+        "Standard",
+        FrontRunnerTrigger() & RandomCornerTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+
+def pace_corners():
+    return Skill(
+        "Pace Chaser Corners",
+        "Standard",
+        PaceChaserTrigger() & RandomCornerTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+def late_corners():
+    return Skill(
+        "Late Surger Corners",
+        "Standard",
+        LateSurgerTrigger() & RandomCornerTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+def end_corners():
+    return Skill(
+        "End Closer Corners",
+        "Standard",
+        EndCloserTrigger() & RandomCornerTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+
+def front_straightaways():
+    return Skill(
+        "Front Runner Straightaways",
+        "Standard",
+        FrontRunnerTrigger() & RandomStraightawayTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+
+def pace_straightaways():
+    return Skill(
+        "Pace Chaser Straightaways",
+        "Standard",
+        PaceChaserTrigger() & RandomStraightawayTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+def late_straightaways():
+    return Skill(
+        "Late Surger Straightaways",
+        "Standard",
+        LateSurgerTrigger() & RandomStraightawayTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+def end_straightaways():
+    return Skill(
+        "End Closer Straightaways",
+        "Standard",
+        EndCloserTrigger() & RandomStraightawayTrigger(),
+        [Velocity(0.5)],
+        duration = 3.0
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### ACCEL ##################################################################################################################################
+############################################################################################################################################
+
+
+
+def corner_acceleration():
+    return Skill(
+        "Corner Acceleration",
+        "Standard",
+        RandomCornerTrigger(),
+        [Acceleration(0.2)],
+        duration = 3.0
+    )
+def corner_connoisseur():
+    return Skill(
+        "Corner Connoisseur",
+        "Rare",
+        RandomCornerTrigger(),
+        [Acceleration(0.4)],
+        duration = 3.0
+    )
+
+def straightaway_acceleration():
+    return Skill(
+        "Straightaway Acceleration",
+        "Standard",
+        RandomStraightawayTrigger(),
+        [Acceleration(0.2)],
+        duration = 3.0
+    )
+
+def rushing_gale():
+    return Skill(
+        "Rushing Gale",
+        "Rare",
+        RandomStraightawayTrigger(),
+        [Acceleration(0.4)],
+        duration = 3.0
+    )
+
+def highlander():
+    return Skill(
+        "Highlander",
+        "Standard",
+        UphillTrigger(),
+        [Acceleration(0.2)],
+        duration=3.0
+    )
+
+def early_lead():
+    return Skill(
+        "Early Lead",
+        "Standard",
+        AfterDistanceTrigger(0),
+        [Acceleration(0.2)],
+        duration=1.5
+    )
+
+def taking_the_lead():
+    return Skill(
+        "Taking the Lead",
+        "Rare",
+        AfterDistanceTrigger(0),
+        [Acceleration(0.4)],
+        duration = 1.5
+    )
+
+
 def straight_descent():
     return Skill(
         "Straight Descent",
@@ -204,9 +397,55 @@ def determined_descent():
         [Acceleration(0.4)],
         duration = 3.0
     )
-### RECOVERY #############################################################
 
-### UNIQUE #############################################################
+
+### RECOVERY ###############################################################################################################################
+############################################################################################################################################
+
+
+
+def corner_recovery():
+    return Skill(
+        "Corner Recovery",
+        "Standard",
+        RandomCornerTrigger(),
+        [Recovery(25)]
+    )
+
+def swinging_maestro():
+    return Skill(
+        "Swinging Maestro",
+        "Rare",
+        RandomCornerTrigger(),
+        [Recovery(100)]
+    )
+
+def straightaway_recovery():
+    return Skill(
+        "Corner Recovery",
+        "Standard",
+        RandomStraightawayTrigger(),
+        [Recovery(25)]
+    )
+
+def breath_of_fresh_air():
+    return Skill(
+        "Breath Of Fresh Air",
+        "Rare",
+        RandomStraightawayTrigger(),
+        [Recovery(100)]
+    )
+
+
+
+
+
+
+### ULTIMATE ###############################################################################################################################
+############################################################################################################################################
+
+
+
 def moving_past_and_beyond():
     return Skill(
         "Moving Past, and Beyond",
@@ -224,3 +463,26 @@ def angling_and_scheming():
         [Acceleration(0.4)],
         duration = 3.0
     )
+
+
+def this_dance_is_for_vittoria():
+    return Skill(
+        "This Dance is for Vittoria !",
+        "Unique",
+        AfterDistanceTrigger(LAST_SPURT) & (OvertakingTrigger() | OvertakenTrigger()),
+        [Velocity(2)],
+        duration=5.0
+    )
+
+def anchors_aweigh():
+    return Skill(
+        "Anchors Aweigh",
+        "Unique",
+        AfterDistanceTrigger(50) & AfterPositionTrigger(0.75),
+        [Velocity(1.5)],
+        duration=7.5
+    )
+
+### CHANTIER ###############################################################################################################################
+############################################################################################################################################
+
